8f8948cbb718fdce482103ccc0b77f719e7ca87f
0gl04q <0gl04q@mail.ru>
2024-11-24 17:38:50 +0300

    Update settings: add data path for database_url

--- settings/config.py
+++ settings/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     BASE_URL: str = '127.0.0.1:8000'
     HTTP_SCHEME: str = 'http'
-    DATABASE_URL: str = f'sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3'
+    DATABASE_URL: str = f'sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3'
     GENERATE_LIST: list = list(ascii_lowercase + ascii_uppercase + digits)
     REDIS_URL: str = 'redis://localhost:6379'
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
