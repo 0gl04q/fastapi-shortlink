5424945350ad0a41446eee079cdf783857ddf1d2
0gl04q <0gl04q@mail.ru>
2024-11-24 20:42:16 +0300

    Update frontend

--- static/js/index.js
+++ static/js/index.js
@@ -4,47 +4,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const result = document.querySelector('.result');
     const resultDisplay = document.querySelector('.result-display');
     const copyBtn = document.querySelector('.copy-btn');
-    const errorMessage = document.querySelector('.error-message');
 
-    // Enable/Disable the button based on URL validity
     urlInput.addEventListener('input', function () {
         try {
             new URL(this.value);
             shortenBtn.disabled = false;
-            errorMessage.style.display = 'none';
+            result.classList.remove('active');
         } catch {
             shortenBtn.disabled = true;
+            result.classList.remove('active');
         }
     });
 
-    // Send POST request to generate short URL
     shortenBtn.addEventListener('click', async function () {
         if (!urlInput.value) return;
 
-        try {
-            const response = await fetch('/generate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({url: urlInput.value}),
-            });
+        const endpoint = `/generate?url=${encodeURIComponent(urlInput.value)}`;
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.detail || 'Something went wrong');
-            }
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+            },
+            body: '',
+        });
 
-            const shortUrl = await response.text();
-            resultDisplay.value = shortUrl;
-            result.classList.add('active');
-        } catch (err) {
-            errorMessage.style.display = 'block';
-            errorMessage.textContent = err.message;
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.detail || 'Something went wrong');
         }
+
+        const shortUrl = await response.text();
+        resultDisplay.value = shortUrl.slice(1, -1);
+        result.classList.add('active');
     });
 
-    // Copy functionality
     copyBtn.addEventListener('click', function () {
         resultDisplay.select();
         navigator.clipboard.writeText(resultDisplay.value).then(() => {
